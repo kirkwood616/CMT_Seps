c6218118937061ea509efa0c54a0b99250c38e94
kirkwood616 <kirkwood616@gmail.com>
2024-01-11 19:05:08 -0500

    added info alerts for action sets

---
+++ RTF/RTF_Alert_ArtPrep.jsx
@@ -0,0 +1,81 @@
+function RTF_Alert_ArtPrep() {
+  // Art Prep Info
+  alert(
+    "RTF Art Prep Info" +
+      "\n" +
+      "These actions will help to clean up and identify potential problems or mistakes in the artwork." +
+      "\n\n" +
+      "Run these actions in the order they are listed." +
+      "\n\n" +
+      "Failing to run these steps can make the steps later in the process fail, have undesired results or innacurate separation info."
+  );
+
+  // Delete Hidden Paths
+  alert("Delete Hidden Paths" + "\n" + "Remove any paths that are hidden/not visible within the pasted artwork.");
+
+  // Delete No Color Paths
+  alert("Delete No Color Paths" + "\n" + "Deletes any paths with a fill of [None]");
+
+  // Convert CMYK Swatches
+  alert(
+    "Convert CMYK Swatches" +
+      "\n" +
+      "Converts any Process Color found in the swatch panel to a Spot Color." +
+      "\n\n" +
+      "Any paths that were previously colored with the Process swatch will be changed to the Spot swatch."
+  );
+
+  // CMYK No Swatch Check
+  alert(
+    "CMYK No Swatch Check" +
+      "\n" +
+      "Check for any CMYK/Process colors in the artwork without a swatch." +
+      "\n\n" +
+      "If present, it will create a Spot Swatch beginning with the name 'PROCESS SWATCH' in the Swatch panel, as well as recolor the paths to that Spot color." +
+      "\n\n" +
+      "Either rename the 'PROCESS SWATCH' to keep it in the artwork, or run the next action 'Delete Converted CMYK Swatches' to delete the swatch and paths."
+  );
+
+  // Delete Converted CMYK Swatches
+  alert(
+    "Delete Converted CMYK Swatches" +
+      "\n" +
+      "Deletes all Swatches beginning with the name 'PROCESS COLOR', as well as any paths colored with that swatch."
+  );
+
+  // Delete Unused Swatches
+  alert(
+    "Delete Converted CMYK Swatches" +
+      "\n" +
+      "Deletes all unused swatches." +
+      "\n\n" +
+      "If unused swatches are found, a prompt will appear asking you to confirm your choice." +
+      "\n\n" +
+      "If no unused swatches are found, nothing will appear."
+  );
+
+  // Check Opacity < 100%
+  alert(
+    "Check Opacity < 100%" +
+      "\n" +
+      "Finds and selects any items with less than 100% opacity." +
+      "\n\n" +
+      "Opacity can be changed via the Transparency Window."
+  );
+
+  // Check Color Tints < 100%
+  alert(
+    "Check Color Tints < 100%" +
+      "\n" +
+      "Finds and selects any items with less than 100% color tint." +
+      "\n\n" +
+      "Color tint can be changed via the Color Window."
+  );
+}
+
+// Run
+try {
+  RTF_Alert_ArtPrep();
+} catch (e) {
+  alert(e, "Script Alert", true);
+}
---
+++ RTF/RTF_Alert_FirstSteps.jsx
@@ -0,0 +1,50 @@
+function RTF_Alert_FirstSteps() {
+  // First Steps Info
+  alert(
+    "RTF First Steps Info" +
+      "\n" +
+      "First steps of the automation process." +
+      "\n\n" +
+      "Do not skip these steps, especially Order & Art Info & Paste Art."
+  );
+
+  // Open Template
+  alert(
+    "Open Template" +
+      "\n" +
+      "Automatically open the RTF Template in a new window." +
+      "\n\n" +
+      "This can only be ran when a document is open or an error will be displayed." +
+      "\n\n" +
+      "You must edit the 'RTF_Open_Template.jsx' file and change the file path to where you have your template file located." +
+      "\n\n" +
+      "Editing this file can be done in a text editor (i.e. TextEdit or Notepad)."
+  );
+
+  // Order & Art Info
+  alert(
+    "Order & Art Info" +
+      "\n" +
+      "Prompts for the order & art number." +
+      "\n\n" +
+      "This info will be placed info the Metadata layer and the art number will be used to automatically save a file later in the process." +
+      "\n\n" +
+      "Input is not case sensitive. All input will be capitalized."
+  );
+
+  // Paste Art
+  alert(
+    "Paste Art" +
+      "\n" +
+      "Pastes the copied artwork 0.50 inches from the top registration and centered to artboard" +
+      "\n\n" +
+      "Ungroups everything in the pasted artwork. This is necessary for the next steps in the process."
+  );
+}
+
+// Run
+try {
+  RTF_Alert_FirstSteps();
+} catch (e) {
+  alert(e, "Script Alert", true);
+}
