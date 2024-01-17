13091a452301cd42828ac5050b3d83e6277f28c3
kirkwood616 <kirkwood616@gmail.com>
2024-01-16 23:52:13 -0500

    refactored to single gui window

--- RTF/RTF_OrderArtPrompt.jsx
+++ RTF/RTF_OrderArtPrompt.jsx
@@ -11,19 +11,61 @@ function RTF_OrderArtPrompt() {
   var orderNameText = orderName.contents.replace("ORDER ", "");
   var artNameText = artName.contents.replace("ART FILE: ", "");
 
-  // Prompts to get Order Number & Art File Names
-  var orderNumber = prompt("Enter Order Number", orderNameText, "Order Number");
-  var artFile = prompt("Enter Art File", artNameText, "Order Number").toUpperCase();
+  // GUI Window
+  var gui = new Window("dialog", "SET ORDER & ART NUMBER");
+  gui.orientation = "column";
+  gui.preferredSize.width = 300;
 
-  // Error handling for if no value or CANCEL button is selected
-  if (!orderNumber || !artFile) {
-    alert("Value(s) Missing\nRe-Run Script & Enter Values");
-    return;
-  }
+  // Order Panel
+  var orderPanel = createPanel(gui, "Order Number");
+  // Order Panel Input
+  var orderInput = orderPanel.add("edittext", undefined, orderNameText);
+  orderInput.active = true;
 
-  // Set Order & Art File Text
-  orderName.contents = "ORDER " + orderNumber;
-  artName.contents = "ART FILE: " + artFile;
+  // Art Panel
+  var artPanel = createPanel(gui, "Art Number");
+  // Art Panel Input
+  var artInput = artPanel.add("edittext", undefined, artNameText);
+
+  // Button Control Group
+  var buttonGroup = createGroup(gui, "row");
+  buttonGroup.alignChildren = "fill";
+
+  // Cancel Button
+  var cancelButton = createButton(buttonGroup, "CANCEL", function () {
+    gui.close();
+  });
+
+  // OK Button
+  var okButton = createButton(buttonGroup, "OK", function () {
+    // Check for no input & alert
+    if (!orderInput.text || !artInput.text) {
+      if (!orderInput.text && !artInput.text) {
+        alert("No Numbers Entered" + "\n" + "Enter Numbers");
+        orderInput.active = true;
+      }
+
+      if (!orderInput.text && artInput.text) {
+        alert("No Order Number Entered" + "\n" + "Enter Order Number");
+        orderInput.active = true;
+      }
+
+      if (orderInput.text && !artInput.text) {
+        alert("No Art Number Entered\nEnter Art Number");
+        artInput.active = true;
+      }
+    }
+
+    // Set textframes contents & close window
+    if (orderInput.text && artInput.text) {
+      orderName.contents = "ORDER " + orderInput.text;
+      artName.contents = artInput.text.toUpperCase();
+      gui.close();
+    }
+  });
+
+  // Show GUI
+  gui.show();
 }
 
 // Run
@@ -35,4 +77,31 @@ try {
   }
 } catch (e) {
   alert(e, "Script Alert", true);
+}
+
+//*******************
+// Helper functions
+//*******************
+
+// Create Window Panel
+function createPanel(parent, title) {
+  var panel = parent.add("panel", undefined, title);
+  panel.orientation = "column";
+  panel.alignChildren = "fill";
+  panel.preferredSize.width = 250;
+  return panel;
+}
+
+// Create Window Group
+function createGroup(parent, orientation) {
+  var group = parent.add("group");
+  group.orientation = orientation;
+  return group;
+}
+
+// Create Window Button
+function createButton(parent, title, onClick) {
+  var button = parent.add("button", undefined, title);
+  if (onClick !== undefined) button.onClick = onClick;
+  return button;
 }
