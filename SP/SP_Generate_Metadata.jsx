d64aeb1f79c42368e8d62ed553196b6ec75e52ea
kirkwood616 <kirkwood616@gmail.com>
2024-01-25 23:34:34 -0500

    added Generate_Metadata

---
+++ SP/SP_Generate_Metadata.jsx
@@ -0,0 +1,96 @@
+function SP_Generate_Metadata() {
+  // Active Document
+  var doc = app.activeDocument;
+
+  // Swatches
+  var docSwatches = doc.swatches;
+
+  // Metadata Layer items
+  var metadataLayer = doc.layers.getByName("Metadata");
+  var metaTextFrames = metadataLayer.textFrames;
+
+  // Storage
+  var colorFrames = new Array();
+  var swatchStorage = new Array();
+
+  // Unlock Metadata layer
+  metadataLayer.locked = false;
+
+  // Deselect everything
+  doc.selection = false;
+
+  // Add colored text frames to storage
+  for (var i = metaTextFrames.length; i--; ) {
+    if (metaTextFrames[i].name.charAt(0) !== "_") {
+      colorFrames.push(metaTextFrames[i]);
+    }
+  }
+
+  // Remove all colored text frames except the first one
+  for (var i = 0; i < colorFrames.length; i++) {
+    if (i !== 0) {
+      colorFrames[i].remove();
+    }
+  }
+
+  // Remove frames from storage
+  colorFrames.length = 1;
+
+  // Add swatches to storage
+  for (var i = 0; i < docSwatches.length; i++) {
+    if (docSwatches[i].name !== "[None]" && docSwatches[i].name !== "[Registration]") {
+      swatchStorage.push(docSwatches[i]);
+    }
+  }
+
+  // Set edited name, contents and fill color
+  for (var i = 0; i < swatchStorage.length; i++) {
+    var nameForMeta = editMetadataText(swatchStorage[i].name);
+
+    colorFrames[i].name = nameForMeta;
+    colorFrames[i].contents = nameForMeta;
+    colorFrames[i].textRange.characterAttributes.fillColor = swatchStorage[i].color;
+
+    // Duplicate text frame if over 1 color
+    if (i !== swatchStorage.length - 1) {
+      var frameCopy = colorFrames[i].duplicate();
+      colorFrames.push(frameCopy);
+    }
+  }
+}
+
+// Run
+try {
+  if (app.documents.length > 0 && app.activeDocument.artboards[0].name === "SP_Template") {
+    SP_Generate_Metadata();
+  } else {
+    throw new Error("SP Template File Not Active");
+  }
+} catch (e) {
+  alert(e, "Script Alert", true);
+}
+
+//*******************
+// Helper functions
+//*******************
+
+/**
+ * Remove unwanted characters from Metadata color text
+ * @param {String} colorName - Spot swatch color name
+ * @returns {String} Text with removed chars
+ */
+function editMetadataText(colorName) {
+  // Remove any forward slashes from text
+  var noForwardSlash = colorName.replace(/\//g, " ");
+
+  // Remove "Spot" from text
+  var noSpot = noForwardSlash.replace(/SPOT /gi, "");
+
+  // Remove "PANTONE " from text
+  var noPantone = noSpot.replace(/PANTONE /gi, "");
+
+  // Remove parenthesis and contents contained between
+  var noParenthesis = noPantone.replace(/\s*\(.*?\)\s*/g, " ");
+
+  return noParenthesis;
+}
