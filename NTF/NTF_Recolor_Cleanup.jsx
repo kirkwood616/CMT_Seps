7d3a9e93ef28808bb1a316bf1f1f9f99295f8c09
kirkwood616 <kirkwood616@gmail.com>
2024-06-25 18:54:51 -0400

    added Recolor_Cleanup

---
+++ NTF/NTF_Recolor_Cleanup.jsx
@@ -0,0 +1,32 @@
+function NTF_Recolor_Cleanup() {
+  // Active Document
+  var doc = app.activeDocument;
+  var docLayers = doc.layers;
+
+  // Deselect everything
+  doc.selection = null;
+
+  // Delete empty layers
+  for (var i = 0; i < docLayers.length; i++) {
+    if (docLayers[i].pageItems.length < 1) docLayers[i].remove();
+  }
+
+  // Recolor everything on a layer to the swatch matching it's name
+  for (var i = 3; i < docLayers.length; i++) {
+    var layerSwatch = doc.swatches.getByName(docLayers[i].name);
+    docLayers[i].hasSelectedArtwork = true;
+    doc.defaultFillColor = layerSwatch.color;
+    doc.selection = null;
+  }
+}
+
+// Run
+try {
+  if (app.documents.length > 0 && app.activeDocument.artboards[0].name !== "NTF_Template") {
+    NTF_Recolor_Cleanup();
+  } else {
+    throw new Error("Template File Not Active");
+  }
+} catch (e) {
+  alert(e + "\n\n" + "Error Code: " + e.line, "Script Alert", true);
+}
