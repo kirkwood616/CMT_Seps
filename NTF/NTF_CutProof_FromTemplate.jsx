15b11a1fa39a5d494a27e7a827d46b88e8048d59
kirkwood616 <kirkwood616@gmail.com>
2024-06-25 07:10:59 -0400

    added CutProof_FromTemplate

---
+++ NTF/NTF_CutProof_FromTemplate.jsx
@@ -0,0 +1,231 @@
+function NTF_CutProof_FromTemplate() {
+  // Active NTF Template Document
+  var templateDoc = app.activeDocument;
+
+  // De-select everything
+  templateDoc.selection = null;
+
+  // Layers
+  var templateLayers = templateDoc.layers;
+
+  // Store for named color layers in NTF Template Document
+  var templateColorLayers = new Array();
+
+  for (var i = templateLayers.length; i--; ) {
+    if (templateLayers[i].name.indexOf("_") !== -1) {
+      templateColorLayers.push(templateLayers[i]);
+    }
+  }
+
+  // Path for Cut Proof Template
+  var proofDocPath = "~/Desktop/Illustrator_Scripts/CMT_Seps/templates/NTF_Cut_Proof.ait";
+
+  // Open Cut Proof Template
+  app.open(new File(proofDocPath));
+
+  // NTF Cut Proof Template
+  var proofDoc = app.activeDocument;
+  proofDoc.selection = null;
+
+  // Prompt for Due Date & set
+  var dueDate = proofDoc.textFrames.getByName("DUE_DATE");
+  dueDate.contents = dateToday();
+  var newDueDate = prompt("ENTER DUE DATE", dateToday(), "DUE DATE");
+
+  if (newDueDate) {
+    dueDate.textRange.contents = newDueDate;
+  }
+
+  // Extract Metadata from Template & set on Proof
+  /// TEMPLATE Meta
+  var templateMetaLayer = templateDoc.layers.getByName("Metadata");
+  var templateMetaGroup = templateMetaLayer.groupItems.getByName("MetaGroup");
+  var templateOrderName = templateMetaGroup.textFrames.getByName("_ORDER");
+  var templateArtName = templateMetaGroup.textFrames.getByName("_ART FILE");
+  var templateColorFrame = templateMetaGroup.textFrames.getByName("COLOR");
+
+  //// PROOF Meta
+  var proofOrderNumber = proofDoc.textFrames.getByName("ORDER_NUMBER");
+  proofOrderNumber.contents = templateOrderName.contents.replace("ORDER ", "");
+
+  var proofArtNumber = proofDoc.textFrames.getByName("ART_NUMBER");
+  proofArtNumber.contents = templateArtName.contents.replace("ART FILE: ", "");
+
+  var proofInkColor = proofDoc.textFrames.getByName("INK_COLOR");
+  proofInkColor.contents = getColorName(templateColorFrame.contents);
+
+  //// PROOF Artboard
+  var proofArtboardIndex = proofDoc.artboards.getActiveArtboardIndex();
+  var proofBoard = proofDoc.artboards[proofArtboardIndex];
+  var proofRect = proofBoard.artboardRect;
+  var offsetH = 20;
+
+  //// PROOF Background
+  var proofBackgroundLayer = proofDoc.layers.getByName("BACKGROUND");
+  var proofBackground = proofBackgroundLayer.pathItems.getByName("_BG");
+  var proofBackgroundPosition = proofBackground.position;
+  var proofBackgroundDimensions = [proofBackground.width, proofBackground.height];
+
+  // Duplicate artboards based on color layers amount
+  if (templateColorLayers.length > 1) {
+    proofDoc.selectObjectsOnActiveArtboard();
+    app.copy();
+  }
+
+  for (var i = 0; i < templateColorLayers.length; i++) {
+    if (i === 0) continue;
+    var newBoard = proofDoc.artboards.add(proofRect);
+    var lastBoard = proofDoc.artboards[i - 1];
+    var lastRect = lastBoard.artboardRect;
+    newBoard.artboardRect = [
+      lastRect[2] + offsetH,
+      lastRect[1],
+      lastRect[2] + offsetH + (proofRect[2] - proofRect[0]),
+      lastRect[3],
+    ];
+    newBoard.name = (i + 1).toString();
+    app.executeMenuCommand("pasteBack");
+    proofDoc.selection = null;
+  }
+
+  if (templateColorLayers.length > 1) {
+    for (var i = proofDoc.pathItems.length; i--; ) {
+      if (proofDoc.pathItems[i].name === "_BG") {
+        proofDoc.pathItems[i].move(proofBackgroundLayer, ElementPlacement.PLACEATEND);
+      }
+    }
+  }
+
+  // Copy color layers from template and place on Cut Proof
+  var proofLayerNames = proofDoc.layers.getByName("NAMES");
+
+  // Stroke color for border box
+  var borderStroke = new CMYKColor();
+  borderStroke.cyan = 0;
+  borderStroke.magenta = 100;
+  borderStroke.yellow = 100;
+  borderStroke.black = 0;
+
+  // Copy, resize, place & position art w/ stroked borders from template to proof artboards
+  for (var i = 0; i < templateColorLayers.length; i++) {
+    // Copy & Paste
+    app.activeDocument = templateDoc;
+    templateColorLayers[i].hasSelectedArtwork = true;
+    app.copy();
+    templateDoc.selection = null;
+    app.activeDocument = proofDoc;
+    proofDoc.activeLayer = proofLayerNames;
+    proofDoc.artboards.setActiveArtboardIndex(i);
+    app.executeMenuCommand("pasteFront");
+
+    // Add cut borders
+    var sel = proofDoc.selection;
+
+    for (var j = sel.length; j--; ) {
+      var itemPosition = sel[j].position;
+      var itemHeight = sel[j].height;
+      var itemWidth = sel[j].width;
+      var borderBox = proofLayerNames.pathItems.rectangle(0, 0, itemWidth + 36, itemHeight + 36);
+      borderBox.move(proofLayerNames, ElementPlacement.PLACEATEND);
+      borderBox.filled = false;
+      borderBox.stroked = true;
+      borderBox.strokeWidth = 2;
+      borderBox.strokeColor = borderStroke;
+      borderBox.strokeDashes = [8];
+      borderBox.position = [itemPosition[0] - 18, itemPosition[1] + 18];
+      borderBox.selected = true;
+    }
+
+    // Group & target current art
+    app.executeMenuCommand("group");
+    var pastedArt = proofDoc.selection[0];
+
+    // Resize art to 7.5" wide if over
+    if (pastedArt.width > 540) {
+      var xScaleBy = (540 / pastedArt.width) * 100;
+      pastedArt.resize(xScaleBy, xScaleBy);
+    }
+
+    // Resize art to 9.25" high if over
+    if (pastedArt.height > 666) {
+      var yScaleBy = (666 / pastedArt.height) * 100;
+      pastedArt.resize(yScaleBy, yScaleBy);
+    }
+
+    // Calculate remainder space between selection & background
+    var yPosBG = proofBackgroundPosition[1];
+    var heightBG = proofBackgroundDimensions[1];
+    var heightRemainder = heightBG - proofDoc.selection[0].height;
+
+    // Position selection centered in background
+    var artboard_x = proofDoc.artboards[i].artboardRect[0] + proofDoc.artboards[i].artboardRect[2];
+    proofDoc.selection[0].position = [(artboard_x - proofDoc.selection[0].width) / 2, yPosBG - heightRemainder / 2];
+
+    // De-select everything
+    proofDoc.selection = null;
+  }
+
+  // Set Metadata Sheet Counts
+  for (var i = 0; i < proofDoc.artboards.length; i++) {
+    proofDoc.artboards.setActiveArtboardIndex(i);
+    proofDoc.selectObjectsOnActiveArtboard();
+
+    var theSel = proofDoc.selection;
+    for (var j = 0; j < theSel.length; j++) {
+      if (theSel[j].name === "SHEET_COUNT") {
+        theSel[j].contents = (i + 1).toString() + " OF " + proofDoc.artboards.length.toString();
+        proofDoc.selection = null;
+        break;
+      }
+    }
+  }
+
+  // De-select everything
+  proofDoc.selection = null;
+}
+
+// Run
+try {
+  if (app.documents.length > 0 && app.activeDocument.artboards[0].name === "NTF_Template") {
+    NTF_CutProof_FromTemplate();
+  } else {
+    throw new Error("Template File Not Active");
+  }
+} catch (e) {
+  alert(e + "\n\n" + "Error Code: " + e.line, "Script Alert", true);
+}
+
+//*******************
+// Helper functions
+//*******************
+
+/**
+ *
+ * @returns {String} Todays date formatted in M/DD or MM/DD
+ */
+function dateToday() {
+  var date = new Date();
+  var todaysDate =
+    (date.getMonth() > 8 ? date.getMonth() + 1 : date.getMonth() + 1) +
+    "/" +
+    (date.getDate() > 9 ? date.getDate() : date.getDate());
+
+  return todaysDate;
+}
+
+/**
+ * Takes a provided string, from ink color Metadata, trims the string to characters preceding an underscore (if present) and returns the trimmed string.
+ * @param {String} name
+ * @returns {String}
+ */
+function getColorName(name) {
+  var thisColor = name;
+
+  var _index = name.indexOf("_");
+
+  if (_index !== -1) {
+    thisColor = name.slice(0, _index);
+  }
+
+  return thisColor;
+}
