129d60da8209972bed83aff1018d08a84f426f88
kirkwood616 <kirkwood616@gmail.com>
2024-06-30 20:02:44 -0400

    added CutProof_SaveFile

---
+++ NTF/NTF_CutProof_SaveFile.jsx
@@ -0,0 +1,120 @@
+#include 'json2.js';
+
+function NTF_CutProof_SaveFile() {
+  // System
+  var system = $.os.substring(0, 3);
+  var slash;
+
+  if (system === "Mac") {
+    slash = "/";
+  }
+  if (system === "Win") {
+    slash = "\\";
+  }
+
+  // Set up & load settings
+  var settingsFile = setupSettingsFile("CMT_Seps_Settings", "Settings_Config.json");
+  var settingsData = loadJSONData(settingsFile);
+  var destinationFolder = settingsData.NTF_proofTemplateSavePath;
+
+  // Active Document
+  var doc = app.activeDocument;
+
+  // Target Metadata layer, Order & Art File text frames
+  var metadataLayer = doc.layers.getByName("Metadata");
+  var orderNumber = metadataLayer.textFrames.getByName("ORDER_NUMBER").contents;
+  var inkName = getColorName(metadataLayer.textFrames.getByName("INK_COLOR").contents);
+
+  // Create file name with appropriate ending
+  var fileName = orderNumber + " " + inkName + " (CP)" + ".ai";
+
+  try {
+    // Save file
+    doc.saveAs(new File(destinationFolder + slash + fileName));
+
+    // Alert
+    alert("File Saved" + "\n" + destinationFolder + slash + fileName);
+  } catch (e) {
+    throw new Error(e.message);
+  }
+}
+
+// Run
+try {
+  if (app.documents.length > 0 && app.activeDocument.artboards[0].name === "CP_1") {
+    NTF_CutProof_SaveFile();
+  } else {
+    throw new Error("Template File Not Active");
+  }
+} catch (e) {
+  alert(e + "\n\n" + "Error Code: " + e.line, "Script Alert", true);
+}
+
+//*******************
+// Helper functions
+//*******************
+
+/**
+ * Uses supplied folder and file name to pull settings from, or creates folder & file if they don't exist.
+ * @param {string} folderName Name of folder
+ * @param {string} fileName Name of file
+ * @returns {File}
+ */
+function setupSettingsFile(folderName, fileName) {
+  var settingsFolderPath = Folder.myDocuments + "/" + folderName;
+  var settingsFolder = new Folder(settingsFolderPath);
+
+  try {
+    if (!settingsFolder.exists) {
+      throw new Error("Settings folder doesn't exist." + "\n" + "Run 'CMT SEPS SETTINGS' and save your settings.");
+    }
+
+    var settingsFilePath = settingsFolder + "/" + fileName;
+    var settingsFile = new File(settingsFilePath);
+
+    if (!settingsFile.exists) {
+      throw new Error("Settings file doesn't exist." + "\n" + "Run 'CMT SEPS SETTINGS' and save your settings.");
+    }
+
+    return new File(settingsFilePath);
+  } catch (e) {
+    throw new Error(e.message);
+  }
+}
+
+/**
+ * Parses a JSON file and returns the data as an object.
+ * @param {File}      file JSON file
+ * @returns {Object}  Parsed JSON data
+ */
+function loadJSONData(file) {
+  if (file.exists) {
+    try {
+      file.encoding = "UTF-8";
+      file.open("r");
+      var data = file.read();
+      file.close();
+      data = JSON.parse(data);
+      return data;
+    } catch (e) {
+      throw new Error("Error loading Settings file.");
+    }
+  }
+}
+
+/**
+ * Takes a provided string, from ink color Metadata, trims the string to characters preceeding an underscore (if present) and returns the trimmed string.
+ * @param {String} name
+ * @returns {String}
+ */
+function getColorName(name) {
+  var thisColor = name;
+
+  var _index = name.indexOf("_");
+
+  if (_index !== -1) {
+    thisColor = name.slice(0, _index);
+  }
+
+  return thisColor;
+}
