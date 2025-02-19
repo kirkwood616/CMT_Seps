f6957538eb3c633e65bb18f52e96af3ec06ed923
kirkwood616 <kirkwood616@gmail.com>
2025-02-19 12:58:26 -0500

    updated Polyfills

--- UTILITIES/Polyfills.js
+++ UTILITIES/Polyfills.js
@@ -1,19 +1,40 @@
-// Polyfills to make scripting more like ES6 syntax
+//***********
+// ARRAYS ***
+//***********
 
-Array.prototype.filter = function (callback) {
-  var filtered = [];
-  for (var i = 0; i < this.length; i++) if (callback(this[i], i, this)) filtered.push(this[i]);
-  return filtered;
-};
+/* filter polyfill: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter */
+if (!Array.prototype.filter) {
+  (function () {
+    Array.prototype.filter = function (callback) {
+      var filtered = [];
+      for (var i = 0; i < this.length; i++) if (callback(this[i], i, this)) filtered.push(this[i]);
+      return filtered;
+    };
+  })();
+}
 
-Array.prototype.forEach = function (callback) {
-  for (var i = 0; i < this.length; i++) callback(this[i], i, this);
-};
+/* forEach polyfill: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach */
+if (!Array.prototype.forEach) {
+  (function () {
+    Array.prototype.forEach = function (callback) {
+      for (var i = 0; i < this.length; i++) callback(this[i], i, this);
+    };
+  })();
+}
 
-Array.prototype.includes = function (item) {
-  for (var i = 0; i < this.length; i++) if (this[i] == item) return true;
-  return false;
-};
+/* includes polyfill: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/includes */
+if (!Array.prototype.includes) {
+  (function () {
+    Array.prototype.includes = function (item) {
+      for (var i = 0; i < this.length; i++) if (this[i] == item) return true;
+      return false;
+    };
+  })();
+}
+
+//************
+// STRINGS ***
+//************
 
 /* trim ployfill: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/Trim */
 if (!String.prototype.trim) {
