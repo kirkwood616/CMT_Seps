3207233a5c24fb7038a99421a1eeb4665ad16bdc
kirkwood616 <kirkwood616@gmail.com>
2026-02-04 23:19:03 -0500

    added toPureWhite function

--- CMT_SCRIPTS/UTILITIES/Colors.jsx
+++ CMT_SCRIPTS/UTILITIES/Colors.jsx
@@ -13,3 +13,37 @@ function convertRGBtoCMYK(rgbValues) {
     ColorConvertPurpose.defaultpurpose
   );
 }
+
+/**
+ * Changes the color of all spot swatches in the document with 'White' in the name to pure white.
+ */
+function toPureWhite() {
+  for (var i = 0; i < app.activeDocument.spots.length; i++) {
+    var whiteName = /white/i;
+    if (!whiteName.test(app.activeDocument.spots[i].name)) continue;
+
+    switch (app.activeDocument.spots[i].spotKind) {
+      case SpotColorKind.SPOTCMYK:
+        var newCMYK = new CMYKColor();
+        newCMYK.cyan = newCMYK.magenta = newCMYK.yellow = newCMYK.black = 0;
+        app.activeDocument.spots[i].color = newCMYK;
+        break;
+
+      case SpotColorKind.SPOTLAB:
+        var newLAB = new LabColor();
+        newLAB.l = 100;
+        newLAB.a = newLAB.b = 0;
+        app.activeDocument.spots[i].color = newLAB;
+        break;
+
+      case SpotColorKind.SPOTRGB:
+        var newRGB = new RGBColor();
+        newRGB.red = newRGB.green = newRGB.blue = 255;
+        app.activeDocument.spots[i].color = newRGB;
+        break;
+
+      default:
+        break;
+    }
+  }
+}
